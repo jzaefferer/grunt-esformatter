b6c37bd639c7208be71d2b5a7b3097b8a4e197d5
Andrée Hansson <peolanha@gmail.com>
2014-07-10 00:59:04 +0200

    Update expected output from jquery preset (indentation)

--- test/expected/jquery-preset.js
+++ test/expected/jquery-preset.js
@@ -11,16 +11,16 @@ var esformatter = require( 'esformatter' );
 
 module.exports = function( grunt ) {
 
-grunt.registerMultiTask( 'esformatter', 'Format JS files', function() {
-	var options = this.options({});
-	this.files.forEach(function( f ) {
-		f.src.filter(function( file ) {
-			return grunt.file.exists( file );
-		}).forEach(function( file ) {
-			var content = grunt.file.read( file );
-			grunt.file.write( file, esformatter.format( content, options ) );
+	grunt.registerMultiTask( 'esformatter', 'Format JS files', function() {
+		var options = this.options({});
+		this.files.forEach(function( f ) {
+			f.src.filter(function( file ) {
+				return grunt.file.exists( file );
+			}).forEach(function( file ) {
+				var content = grunt.file.read( file );
+				grunt.file.write( file, esformatter.format( content, options ) );
+			});
 		});
 	});
-});
 
 };
